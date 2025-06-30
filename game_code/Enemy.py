5add2fc8fed97f6833336a142ea02df4339e6505
Roger Yamassaki <skarefes@gmail.com>
2025-06-30 20:07:28 -0300

    Atualizando personagem

---
+++ game_code/Enemy.py
@@ -0,0 +1,12 @@
+from game_code.Const import ENTITY_SPEED, WIN_WIDTH
+from game_code.Entity import Entity
+
+
+class Enemy(Entity):
+    def __init__(self, name: str, position: tuple):
+        super().__init__(name, position)
+
+    def move(self):
+        self.rect.centerx -= ENTITY_SPEED[self.name]
+        if self.rect.top <= 0:
+            self.rect.left = WIN_WIDTH
